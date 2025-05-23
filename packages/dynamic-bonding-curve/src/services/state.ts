1737e287b20112ca530da1ffdbbb2ff0eaf4cd6e
Dann Wee <dannweezj@gmail.com>
2025-05-23 16:34:34 +0800

    fix: comments

--- packages/dynamic-bonding-curve/src/services/state.ts
+++ packages/dynamic-bonding-curve/src/services/state.ts
@@ -100,9 +100,7 @@ export class StateService extends DynamicBondingCurveProgram {
     async getPoolByBaseMint(
         baseMint: PublicKey | string
     ): Promise<ProgramAccount<VirtualPool> | null> {
-        const filters = baseMint
-            ? createProgramAccountFilter(baseMint, 136)
-            : []
+        const filters = createProgramAccountFilter(baseMint, 136)
         const pools = await this.program.account.virtualPool.all(filters)
         return pools.length > 0 ? pools[0] : null
     }
